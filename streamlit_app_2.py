1c08c31269653f9cc64f64954151bcbd57c2e5e1
keeyes <72693591+keeyes@users.noreply.github.com>
2023-03-29 15:45:28 +0800

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -4,11 +4,11 @@ import snowflake.connector as sc
 st.title("Zena's Amazing Athleisure Catalog")
 st.text('Pick a sweatsuit colour or style')
 
-my_cnx = sc.connect(**streamlit.secrets["snowflake"])
+my_cnx = sc.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
 my_data_row = my_cur.fetchall()
 
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
